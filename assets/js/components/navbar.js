15f8ca7b4640f465f4dddba178d63e8d6b8c77ae
diariolunar <diariolunar47@gmail.com>
2026-05-14 20:05:40 -0300

    Update navbar.js

--- assets/js/components/navbar.js
+++ assets/js/components/navbar.js
@@ -1,28 +1,53 @@
 export function renderNavbar() {
   return `
     <header class="navbar">
-      <div class="logo">
-        <a href="/index.html">
-          <img src="/assets/images/logo-diario-lunar.png" alt="Diário Lunar">
+      <div class="container navbar-content">
+
+        <a href="/index.html" class="navbar-logo">
+          <img src="/assets/images/logo-diario-lunar2.png" alt="Diário Lunar">
         </a>
+
+        <nav class="navbar-menu">
+          <a href="/index.html">Início</a>
+
+          <a href="/materias.html">
+            Todas as Matérias
+          </a>
+
+          <a href="/categoria.html?tipo=Literatura">
+            Literatura
+          </a>
+
+          <a href="/categoria.html?tipo=Comunidade">
+            Comunidade
+          </a>
+
+          <a href="/categoria.html?tipo=Autores">
+            Autores
+          </a>
+
+          <a href="/categoria.html?tipo=Eventos">
+            Eventos
+          </a>
+
+          <a href="/categoria.html?tipo=Resenhas">
+            Resenhas
+          </a>
+
+          <a href="/categoria.html?tipo=Entrevistas">
+            Entrevistas
+          </a>
+
+          <a href="/categoria.html?tipo=Destaques%20Lunar">
+            Destaques Lunar
+          </a>
+
+          <a href="/sugestao.html">
+            Sugestão de Pauta
+          </a>
+        </nav>
+
       </div>
-
-      <nav class="menu">
-        <a href="/index.html">Início</a>
-        <a href="/materias.html">Todas as Matérias</a>
-        <a href="/categoria.html?tipo=Literatura">Literatura</a>
-        <a href="/categoria.html?tipo=Comunidade">Comunidade</a>
-        <a href="/categoria.html?tipo=Autores">Autores</a>
-        <a href="/categoria.html?tipo=Eventos">Eventos</a>
-        <a href="/categoria.html?tipo=Resenhas">Resenhas</a>
-        <a href="/categoria.html?tipo=Entrevistas">Entrevistas</a>
-        <a href="/categoria.html?tipo=Destaques Lunar">Destaques Lunar</a>
-        <a href="/contato.html">Sugestão de Pauta</a>
-      </nav>
-
-      <a href="/admin.html" class="btn">
-        Área ADM
-      </a>
     </header>
   `;
 }
